e3ebd06b250f34c875fd7d64a9b760f7e30052fa
rochac2lee <cleberli.rocha@gmail.com>
2020-10-22 08:31:21 -0300

    Hellow World Express com url amigável!

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require("express");
 /** Instancia o express */
 var app = express();
 
+/** Diz ao app que vai ser carregado arquivos estáticos */
+app.use(express.static("public"));
+
 /** Fornece a constante dirname para dizer o diretório atual do projeto */
 var path = require("path");
 
 app.get("/", function(request, response) {
     response.sendFile(__dirname + "/public/pages/index.html");
+})
+
+app.get("/sobre", function(request, response) {
+    response.sendFile(__dirname + "/public/pages/sobre.html");
 })
 
 
